2b994fed350b3d7ef800bcefb31c0d553b194c0b
Lakshminarayana Devaraj <ld261v@att.com>
2018-11-14 13:17:26 -0500

    blueprint pod data & chomp
    Change-Id: Ib7f3317fedcf30ee6e4d7b90cf47c56f429c10bd
    Signed-off-by: Lakshminarayana Devaraj <ld261v@att.com>

---
+++ AECPortalMgmt/src/main/java/org/akraino/portal/data/NDiskPartition.java
@@ -0,0 +1,58 @@
+/* 
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.akraino.portal.data;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"name", "size", "mountpoint"})
+public class NDiskPartition {
+
+	private String mountpoint;
+	
+	private String name;
+	private String size;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getMountpoint() {
+		return mountpoint;
+	}
+
+	public void setMountpoint(String mountpoint) {
+		this.mountpoint = mountpoint;
+	}
+
+	@Override
+	public String toString() {
+		return "NDiskPartition [mountPoint=" + mountpoint + ", name=" + name + ", size=" + size + "]";
+	}
+
+}
---
+++ AECPortalMgmt/src/main/java/org/akraino/portal/data/NKSNNode.java
@@ -0,0 +1,73 @@
+/* 
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.akraino.portal.data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class NKSNNode extends NNode {
+
+	@JsonProperty("api_service_ip")
+	private String apiServiceIP;
+
+	@JsonProperty("etcd_service_ip")
+	private String etcdServiceIP;
+
+	@JsonProperty("pod_cidr")
+	private String podCIDR;
+
+	@JsonProperty("service_cidr")
+	private String serviceCIDR;
+
+	public String getApiServiceIP() {
+		return apiServiceIP;
+	}
+
+	public String getEtcdServiceIP() {
+		return etcdServiceIP;
+	}
+
+	public String getPodCIDR() {
+		return podCIDR;
+	}
+
+	public String getServiceCIDR() {
+		return serviceCIDR;
+	}
+
+	public void setApiServiceIP(String apiServiceIP) {
+		this.apiServiceIP = apiServiceIP;
+	}
+
+	public void setEtcdServiceIP(String etcdServiceIP) {
+		this.etcdServiceIP = etcdServiceIP;
+	}
+
+	public void setPodCIDR(String podCIDR) {
+		this.podCIDR = podCIDR;
+	}
+
+	public void setServiceCIDR(String serviceCIDR) {
+		this.serviceCIDR = serviceCIDR;
+	}
+
+	@Override
+	public String toString() {
+		return "NKSNNode [apiServiceIP=" + apiServiceIP + ", etcdServiceIP=" + etcdServiceIP + ", podCIDR=" + podCIDR
+				+ ", serviceCIDR=" + serviceCIDR + "]";
+	}
+
+}
