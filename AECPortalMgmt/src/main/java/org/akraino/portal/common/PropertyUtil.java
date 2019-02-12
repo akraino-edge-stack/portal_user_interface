12878fb45e219bad9512ef408074c9c936f47047
Eby, Robert (re2429) <re2429@att.com>
2019-02-12 15:30:20 -0500

    Fixed display of Akraino logo
    Removed unused ATT logo
    
    Added $IP substitution in PropertyUtil
    
    Change-Id: I2c1afa0edb7fe3da2b9f4d7bdc0fd1774e422a97
    Signed-off-by: Eby, Robert (re2429) <re2429@att.com>

--- AECPortalMgmt/src/main/java/org/akraino/portal/common/PropertyUtil.java
+++ AECPortalMgmt/src/main/java/org/akraino/portal/common/PropertyUtil.java
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,38 +23,53 @@ import org.akraino.portal.config.AppConfig;
 import org.apache.log4j.Logger;
 
 public class PropertyUtil {
-	
 	private static final Logger logger = Logger.getLogger(PropertyUtil.class);
-	
 	private static final String APP_PROPERTIES_FILENAME = "app.properties";
-	
 	private static PropertyUtil propertyUtil;
-	Properties appProps;
-	
-	public static PropertyUtil getInstance() {
+
+	private Properties appProps;
+
+	/**
+	 * Return the single instance of this object in the app.
+	 * @return the singleton
+	 */
+	public static synchronized PropertyUtil getInstance() {
 		if (propertyUtil == null) {
-			return new PropertyUtil();
+			propertyUtil = new PropertyUtil();
 		}
-		
 		return propertyUtil;
 	}
-	
-	private PropertyUtil() {
 
+	private PropertyUtil() {
 		InputStream input = AppConfig.class.getClassLoader().getResourceAsStream(APP_PROPERTIES_FILENAME);
-		
 		appProps = new Properties();
 		try {
 			appProps.load(input);
-			
 		} catch (IOException e) {
-			logger.error("Error loading app properties file");
+			logger.error("Error loading properties file: "+APP_PROPERTIES_FILENAME);
+		} finally {
+			try {
+				input.close();
+			} catch (IOException e) {
+				// ignore
+			}
 		}
 	}
 
+	/**
+	 * Get a property from the PropertyUtil object.  If the environment variable $IP is set,
+	 * then any URL's referring to localhost will be rewritten to use this IP address instead.
+	 * @param key the key to use to find the property
+	 * @return the value
+	 */
 	public String getProperty(String key) {
-		
-		return appProps.getProperty(key);
+		String s = appProps.getProperty(key);
+		if (s != null && s.indexOf("://localhost:") > 0) {
+			String ip = System.getenv().get("IP");
+			if (ip != null && !"".contentEquals(ip)) {
+				s = s.replaceAll("://localhost:", "://"+ip+":");
+			}
+		}
+		return s;
 	}
-	
 }
