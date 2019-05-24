9c8ddf72e1465da288c5b3c4938f78a489489d66
davidplunkett <dp7642@att.com>
2019-05-24 04:28:50 +0000

    add missing import for java.util.Map
    Signed-off-by: davidplunkett <dp7642@att.com>
    Change-Id: I924d8ff0474a6512f0ec499af8f587eeeb20cb57

--- AECPortalMgmt/src/main/java/org/akraino/portal/service/EdgeSiteService.java
+++ AECPortalMgmt/src/main/java/org/akraino/portal/service/EdgeSiteService.java
@@ -24,6 +24,7 @@ import java.nio.file.Files;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.persistence.EntityNotFoundException;
